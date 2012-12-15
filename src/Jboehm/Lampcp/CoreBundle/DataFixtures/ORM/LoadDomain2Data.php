15ec01f56fe43ca886f6b26796b5ad7deb893332
Jeffrey Boehm <post@jeffrey-boehm.de>
2012-12-15 18:39:43 +0100

    New Fixture

---
+++ src/Jboehm/Lampcp/CoreBundle/DataFixtures/ORM/LoadDomain2Data.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Jboehm\Uhcx\CoreBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Jboehm\Lampcp\CoreBundle\Entity\Domain;
+use Jboehm\Lampcp\CoreBundle\Entity\Subdomain;
+use Jboehm\Lampcp\CoreBundle\Entity\SystemUser;
+use Jboehm\Lampcp\CoreBundle\Entity\Protection;
+use Jboehm\Lampcp\CoreBundle\Entity\MailAccount;
+use Jboehm\Lampcp\CoreBundle\Entity\MysqlDatabase;
+use Jboehm\Lampcp\CoreBundle\Entity\PathOption;
+use Jboehm\Lampcp\CoreBundle\Entity\MailAddress;
+
+class LoadDomain2Data implements FixtureInterface {
+	/**
+	 * {@inheritDoc}
+	 */
+	public function load(ObjectManager $manager) {
+		$domain = new Domain();
+		$domain
+			->setDomain('uh.cx')
+			->setPath('/var/www/uh.cx')
+			->setGid(10001)
+			->setHasMail(false)
+			->setHasSSH(true)
+			->setHasWeb(true);
+
+		$manager->persist($domain);
+
+		$systemuser = new SystemUser($domain);
+		$systemuser
+			->setName('user101')
+			->setPassword('test123')
+			->setUid(10001);
+
+		$manager->persist($systemuser);
+
+		$mysqldatabase = new MysqlDatabase($domain);
+		$mysqldatabase
+			->setName('uhcxsql1')
+			->setPassword('test123')
+			->setComment('Testdatenbank');
+
+		$manager->persist($mysqldatabase);
+
+		$manager->flush();
+	}
+}
