bde772fb1ea8976522d28cbee2343c7b9b4acb6e
Jeffrey Böhm <jeffrey.boehm@hdnet.de>
2012-12-28 09:43:52 +0100

    Konfiguration erweitert

--- src/Jboehm/Lampcp/CoreBundle/DependencyInjection/Configuration.php
+++ src/Jboehm/Lampcp/CoreBundle/DependencyInjection/Configuration.php
@@ -28,6 +28,11 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('jboehm_lampcp_core');
 
+		$rootNode
+			->children()
+				->scalarNode('crypt')->isRequired()->cannotBeEmpty()->end()
+			->end();
+
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
