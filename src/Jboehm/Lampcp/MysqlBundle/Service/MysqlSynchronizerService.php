00974c7dad95c6568d66b2ebcb81bad35310d200
Jeffrey Boehm <post@jeffrey-boehm.de>
2013-01-06 15:37:29 +0100

    MysqlSyncService

---
+++ src/Jboehm/Lampcp/MysqlBundle/Service/MysqlSynchronizerService.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * LampCP
+ * https://github.com/jeboehm/LampCP
+ *
+ * Licensed under the GPL Version 2 license
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ */
+
+namespace Jboehm\Lampcp\MysqlBundle\Service;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Bridge\Monolog\Logger;
+use Jboehm\Lampcp\MysqlBundle\Service\MysqlAdminService;
+use Jboehm\Lampcp\CoreBundle\Service\SystemConfigService;
+use Jboehm\Lampcp\CoreBundle\Entity\MysqlDatabaseRepository;
+
+class MysqlSynchronizerService {
+	/** @var EntityManager */
+	protected $_em;
+
+	/** @var Logger */
+	protected $_logger;
+
+	/** @var MysqlAdminService */
+	protected $_mysqladmin;
+
+	/** @var SystemConfigService */
+	protected $_systemconfig;
+
+	/**
+	 * Konstruktor
+	 *
+	 * @param EntityManager                                         $em
+	 * @param \Symfony\Bridge\Monolog\Logger                        $logger
+	 * @param MysqlAdminService                                     $mysqladmin
+	 * @param \Jboehm\Lampcp\CoreBundle\Service\SystemConfigService $systemconfig
+	 */
+	public function __construct(EntityManager $em, Logger $logger, MysqlAdminService $mysqladmin,
+								SystemConfigService $systemconfig) {
+		$this->_em           = $em;
+		$this->_logger       = $logger;
+		$this->_mysqladmin   = $mysqladmin;
+		$this->_systemconfig = $systemconfig;
+	}
+
+	/**
+	 * Get entity repository
+	 *
+	 * @return MysqlDatabaseRepository
+	 */
+	protected function _getRepository() {
+		return $this
+			->_em
+			->getRepository('JboehmLampcpCoreBundle:MysqlDatabase');
+	}
+
+	/**
+	 * Get database prefix
+	 *
+	 * @throws \Exception
+	 * @return string
+	 */
+	protected function _getPrefix() {
+		$prefix = $this->_systemconfig->getParameter('systemconfig.option.mysql.db.prefix');
+
+		if(empty($prefix)) {
+			$msg = '(MysqlSynchronizerService) Empty database prefix!';
+			$this->_logger->err($msg);
+			throw new \Exception($msg);
+		}
+
+		return $prefix;
+	}
+
+	/**
+	 * Delete obsolete MySQL databases
+	 */
+	public function deleteObsoleteDatabases() {
+		foreach($this->_mysqladmin->getDatabases($this->_getPrefix()) as $database) {
+			$mysqldb = $this->_getRepository()->findOneBy(array('name' => $database->getName()));
+
+			if(!$mysqldb) {
+				$this->_logger->alert('(MysqlSynchronizerService) Deleting obsolete database: ' . $database->getName());
+				$this->_mysqladmin->dropDatabase($database);
+			}
+		}
+	}
+
+	/**
+	 * Delete obsolete MySQL users
+	 */
+	public function deleteObsoleteUsers() {
+		foreach($this->_mysqladmin->getUsers($this->_getPrefix()) as $user) {
+			$mysqldb = $this->_getRepository()->findOneBy(array('name' => $user->getUsername()));
+
+			if(!$mysqldb) {
+				$this->_logger->alert('(MysqlSynchronizerService) Deleting obsolete user: ' . $user->getUsername());
+				$this->_mysqladmin->dropUser($user);
+			}
+		}
+	}
+}
