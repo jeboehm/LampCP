e21155f0ef218b442a7078c58b6f4fd0e7876616
Jeffrey Boehm <post@jeffrey-boehm.de>
2013-01-12 12:00:44 +0100

    use cleanup

--- src/Jeboehm/Lampcp/CoreBundle/Controller/LogController.php
+++ src/Jeboehm/Lampcp/CoreBundle/Controller/LogController.php
@@ -10,7 +10,6 @@
 
 namespace Jeboehm\Lampcp\CoreBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
--- src/Jeboehm/Lampcp/CoreBundle/Controller/ProtectionUserController.php
+++ src/Jeboehm/Lampcp/CoreBundle/Controller/ProtectionUserController.php
@@ -11,7 +11,6 @@
 namespace Jeboehm\Lampcp\CoreBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
