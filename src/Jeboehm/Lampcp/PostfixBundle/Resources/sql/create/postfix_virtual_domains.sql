cc06a4440cd0a4981849387e28e205340011af2d
Jeffrey Boehm <post@jeffrey-boehm.de>
2013-01-18 23:13:16 +0100

    Postfix schema

--- src/Jeboehm/Lampcp/PostfixBundle/Resources/sql/create/postfix_virtual_domains.sql
+++ src/Jeboehm/Lampcp/PostfixBundle/Resources/sql/create/postfix_virtual_domains.sql
@@ -1,5 +1,5 @@
 CREATE VIEW postfix_virtual_domains AS
-  SELECT Domain.id, Domain.domain
+  SELECT Domain.id, Domain.domain AS name
   FROM Domain, MailAddress
   WHERE MailAddress.domain_id = Domain.id
   GROUP BY Domain.id
