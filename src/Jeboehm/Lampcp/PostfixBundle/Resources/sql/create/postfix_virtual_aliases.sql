633122f3d4cbafb3e3f390c5883116aec0e8196a
Jeffrey Boehm <post@jeffrey-boehm.de>
2013-01-18 23:34:27 +0100

    Aliases

--- src/Jeboehm/Lampcp/PostfixBundle/Resources/sql/create/postfix_virtual_aliases.sql
+++ src/Jeboehm/Lampcp/PostfixBundle/Resources/sql/create/postfix_virtual_aliases.sql
@@ -1,6 +1,15 @@
 CREATE VIEW postfix_virtual_aliases AS
+(
   SELECT MailForward.id, MailForward.domain_id, CONCAT(MailAddress.address, '@', Domain.domain)
     AS source, MailForward.target AS destination
   FROM MailForward, MailAddress, Domain
   WHERE MailForward.domain_id = Domain.id
     AND MailForward.mailaddress_id = MailAddress.id
+) UNION (
+  SELECT MailAccount.id, MailAccount.domain_id, CONCAT(MailAddress.address, '@', Domain.domain)
+    AS source, CONCAT(MailAddress.address, '@', Domain.domain)
+    AS destination FROM MailAddress, Domain, MailAccount
+  WHERE MailAddress.domain_id = Domain.id
+    AND MailAccount.mailaddress_id = MailAddress.id
+    AND MailAccount.enabled = 1
+)
