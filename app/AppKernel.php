e2948943eac1dfee4c53906b34c0cf5bc2b734f7
Jeffrey Boehm <post@jeffrey-boehm.de>
2013-01-31 08:47:20 +0100

    Bootstrap 2.2.2 (fixes #16)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel {
             new Jeboehm\Lampcp\ConfigBundle\JeboehmLampcpConfigBundle(),
             new Jeboehm\Lampcp\PostfixBundle\JeboehmLampcpPostfixBundle(),
             new Jeboehm\Lampcp\LightyConfigBundle\JeboehmLampcpLightyConfigBundle(),
+            new Jeboehm\Lampcp\BootstrapBundle\JeboehmLampcpBootstrapBundle(),
 		);
 
 		if(in_array($this->getEnvironment(), array('dev', 'test'))) {
