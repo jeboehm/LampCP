475862e6f89bde035adeea4bd5c308ad7d2aa404
Jeffrey Boehm <post@jeffrey-boehm.de>
2013-02-19 21:34:06 +0100

    Lighttpd initial setup command (fixes #19)

---
+++ src/Jeboehm/Lampcp/LightyConfigBundle/Command/GenerateLampcpConfigCommand.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * LampCP
+ * https://github.com/jeboehm/LampCP
+ *
+ * Licensed under the GPL Version 2 license
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ */
+
+namespace Jeboehm\Lampcp\LightyConfigBundle\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Jeboehm\Lampcp\LightyConfigBundle\Service\VhostBuilderService;
+use Jeboehm\Lampcp\LightyConfigBundle\Service\DirectoryBuilderService;
+use Jeboehm\Lampcp\ApacheConfigBundle\Command\GenerateLampcpConfigCommand as BaseGenerateLampcpConfigCommand;
+
+class GenerateLampcpConfigCommand extends BaseGenerateLampcpConfigCommand {
+	/**
+	 * Configure command
+	 */
+	protected function configure() {
+		parent::configure();
+		$this->setName('lampcp:lighty:generatelampcpconfig');
+		$this->setDescription('Generates the lighttpd configuration for LampCP');
+	}
+
+
+	/**
+	 * @return VhostBuilderService
+	 */
+	protected function _getVhostBuilderService() {
+		return $this->getContainer()->get('jeboehm_lampcp_lighty_config_vhostbuilder');
+	}
+
+	/**
+	 * @return DirectoryBuilderService
+	 */
+	protected function _getDirectoryBuilderService() {
+		return $this->getContainer()->get('jeboehm_lampcp_lighty_config_directorybuilder');
+	}
+}
