96b6abe297d5b3f712f0756c575d39c46658d5e8
Jeffrey Boehm <post@jeffrey-boehm.de>
2013-03-03 15:35:21 +0100

    Added KnpMenuBundle Moved some files Tidy up...

---
+++ src/Jeboehm/Lampcp/CoreBundle/Menu/GeneralMenuBuilder.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * LampCP
+ * https://github.com/jeboehm/LampCP
+ *
+ * Licensed under the GPL Version 2 license
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ */
+
+namespace Jeboehm\Lampcp\CoreBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class GeneralMenuBuilder extends ContainerAware {
+	/**
+	 * Build the top menu
+	 *
+	 * @param \Knp\Menu\FactoryInterface $factory
+	 * @param array                      $options
+	 *
+	 * @return \Knp\Menu\ItemInterface
+	 */
+	public function getMenu(FactoryInterface $factory, array $options) {
+		$menu = $factory->createItem('root');
+
+		$menu->addChild('leftnavall.general', array(
+												   'attributes' => array(
+													   'class' => 'nav-header',
+												   )
+											  ));
+
+		$menu->addChild('title.page.systemconfig', array(
+														'route' => 'systemconfig',
+												   ));
+
+		$menu->addChild('title.page.ipaddress', array(
+													 'route' => 'config_ipaddress',
+												));
+
+		$menu->addChild('title.page.certificate', array(
+													   'route' => 'config_certificate',
+												  ));
+
+		$menu->addChild('title.page.domain', array(
+												  'route' => 'config_domain',
+											 ));
+
+		return $menu;
+	}
+}
---
+++ src/Jeboehm/Lampcp/CoreBundle/Menu/TopMenuBuilder.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * LampCP
+ * https://github.com/jeboehm/LampCP
+ *
+ * Licensed under the GPL Version 2 license
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ */
+
+namespace Jeboehm\Lampcp\CoreBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Jeboehm\Lampcp\CoreBundle\Service\DomainselectorService;
+use Jeboehm\Lampcp\CoreBundle\Entity\Domain;
+
+class TopMenuBuilder extends ContainerAware {
+	/**
+	 * Build the top menu
+	 *
+	 * @param \Knp\Menu\FactoryInterface $factory
+	 * @param array                      $options
+	 *
+	 * @return \Knp\Menu\ItemInterface
+	 */
+	public function getMenu(FactoryInterface $factory, array $options) {
+		$menu           = $factory->createItem('root');
+		$domainselector = $menu->addChild($this->_getDomainselectorTitle());
+		$menu->addChild('title.page.logout', array(
+												  'route' => 'fos_user_security_logout'
+											 ));
+
+		foreach($this->_getDomainselectorService()->getDomains() as $domain) {
+			/** @var $domain Domain */
+			$domainselector->addChild($domain->getDomain(), array(
+																 'route'           => 'set_domain',
+																 'routeParameters' => array(
+																	 'id' => $domain->getId(),
+																 ),
+															));
+		}
+
+		return $menu;
+	}
+
+	/**
+	 * Get domainselector service
+	 *
+	 * @return DomainselectorService
+	 */
+	protected function _getDomainselectorService() {
+		$service = $this->container->get('jeboehm_lampcp_core.domainselector');
+
+		return $service;
+	}
+
+	/**
+	 * Get title for domainselector menu
+	 *
+	 * @return string
+	 */
+	private function _getDomainselectorTitle() {
+		if($this->_getDomainselectorService()->getSelected() === null) {
+			$text = 'default.topmenu.no.domain';
+		} else {
+			$text = $this->_getDomainselectorService()->getSelected()->getDomain();
+		}
+
+		return $text;
+	}
+}
