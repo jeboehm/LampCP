8c92c177e0f0109ab081d1e3727590c1d27c158f
Jeffrey Boehm <post@jeffrey-boehm.de>
2013-03-10 10:27:37 +0100

    Autofocus on the username field

---
+++ src/Jeboehm/Lampcp/AuthBundle/Resources/public/js/focus.js
@@ -0,0 +1,12 @@
+/**
+ * LampCP
+ * https://github.com/jeboehm/LampCP
+ *
+ * Licensed under the GPL Version 2 license
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ */
+
+jQuery(document).ready(function () {
+	$('#username').focus();
+});
