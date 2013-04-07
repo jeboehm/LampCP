e3168d5858b7bea29bcbaeeeb560f5a98e85c6b6
Jeffrey Böhm <j.boehm@ressourcenkonflikt.de>
2013-04-07 12:15:09 +0200

    Sortierungsbuttons eingefügt

--- src/Jeboehm/Lampcp/CoreBundle/Resources/public/js/dnszone.js
+++ src/Jeboehm/Lampcp/CoreBundle/Resources/public/js/dnszone.js
@@ -10,16 +10,22 @@
 var dnsrecord_holder = $('div.dnsrecords');
 
 jQuery(document).ready(function () {
-    dnsrecord_holder.find('div.controls').each(function () {
+    dnsrecord_holder.find('div.dnsactions').each(function () {
         var dnsrecord_delete_link = $('<a href="#" class="btn btn-danger">-</a>');
+        var dnsrecord_sort_up = $('<a href="#" class="btn btn-info">↑</a>');
+        var dnsrecord_sort_down = $('<a href="#" class="btn btn-info">↓</a>');
 
-        if($(this).attr('id') != 'addbutton') {
+        if ($(this).attr('id') != 'addbutton') {
             $(this).append(dnsrecord_delete_link);
+            $(this).append(dnsrecord_sort_up);
+            $(this).append(dnsrecord_sort_down);
         }
 
         dnsrecord_delete_link.on('click', function (e) {
+            var container = $(this).parent().parent().parent();
             e.preventDefault();
-            $(this).parent().parent().remove();
+            $(container).remove();
+        });
         });
     });
 
