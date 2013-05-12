2f23137040392863ebe5a0f16020f65b92a8de92
Jeffrey Böhm <j.boehm@ressourcenkonflikt.de>
2013-05-12 22:00:00 +0200

    Crypto Form + Validator

---
+++ src/Jeboehm/Lampcp/SetupBundle/Model/Form/Crypto.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * LampCP
+ * https://github.com/jeboehm/LampCP
+ *
+ * Licensed under the GPL Version 2 license
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ */
+
+namespace Jeboehm\Lampcp\SetupBundle\Model\Form;
+
+use Jeboehm\Lampcp\SetupBundle\Model\Annotation\Form;
+
+/**
+ * Class Crypto
+ *
+ * @package Jeboehm\Lampcp\SetupBundle\Model\Form
+ * @author  Jeffrey Böhm <post@jeffrey-boehm.de>
+ */
+class Crypto extends AbstractForm
+{
+    /**
+     * @Form(message="Framework secret key (random alphanum)", type="ask")
+     */
+    public $secret;
+    /**
+     * @Form(message="Password to encrypt account passwords", type="ask")
+     */
+    public $crypt;
+}
