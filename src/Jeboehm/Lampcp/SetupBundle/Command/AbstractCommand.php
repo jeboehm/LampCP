d6855428dabed566aa125a2a2d8696b74b927180
Jeffrey Böhm <j.boehm@ressourcenkonflikt.de>
2013-05-13 22:53:27 +0200

    Parameters.yml creator

---
+++ src/Jeboehm/Lampcp/SetupBundle/Command/AbstractCommand.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * LampCP
+ * https://github.com/jeboehm/LampCP
+ *
+ * Licensed under the GPL Version 2 license
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ */
+
+namespace Jeboehm\Lampcp\SetupBundle\Command;
+
+use Jeboehm\Lampcp\SetupBundle\Model\Transformer\ParametersYamlTransformer;
+use Jeboehm\Lampcp\SetupBundle\Model\Validator\ValidationResult;
+use Jeboehm\Lampcp\SetupBundle\Service\FormGeneratorService;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\DialogHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class AbstractCommand
+ *
+ * @package Jeboehm\Lampcp\SetupBundle\Command
+ * @author  Jeffrey Böhm <post@jeffrey-boehm.de>
+ */
+abstract class AbstractCommand extends ContainerAwareCommand
+{
+    /** @var OutputInterface */
+    private $_output;
+
+    /**
+     * Get Output
+     *
+     * @return OutputInterface
+     */
+    public function getOutput()
+    {
+        return $this->_output;
+    }
+
+    /**
+     * Set Output
+     *
+     * @param OutputInterface $output
+     *
+     * @return $this
+     */
+    private function setOutput(OutputInterface $output)
+    {
+        $this->_output = $output;
+
+        return $this;
+    }
+
+    /**
+     * Get parameters.yml transformer.
+     *
+     * @return ParametersYamlTransformer
+     */
+    protected function getParametersYamlTransformer()
+    {
+        /** @var ParametersYamlTransformer $transformer */
+        $transformer = $this
+            ->getContainer()
+            ->get('jeboehm_lampcp_setup.model.transformer.parametersyamltransformer');
+
+        return $transformer;
+    }
+
+    /**
+     * Get form generator service.
+     *
+     * @return FormGeneratorService
+     */
+    protected function getFormGenerator()
+    {
+        /** @var FormGeneratorService $service */
+        $service = $this
+            ->getContainer()
+            ->get('jeboehm_lampcp_setup.formgeneratorservice');
+
+        $service
+            ->setOutput($this->getOutput())
+            ->setDialog($this->getDialogHelper());
+
+        return $service;
+    }
+
+    /**
+     * Get dialog helper.
+     *
+     * @return DialogHelper
+     */
+    protected function getDialogHelper()
+    {
+        return $this
+            ->getHelperSet()
+            ->get('dialog');
+    }
+
+    /**
+     * Check result. Output error message.
+     *
+     * @param ValidationResult $result
+     *
+     * @return bool
+     */
+    protected function checkResult(ValidationResult $result)
+    {
+        if (!$result->getSuccessful()) {
+            if (!is_array($result->getMessage())) {
+                $messages = array($result->getMessage());
+            } else {
+                $messages = $result->getMessage();
+            }
+
+            foreach ($messages as $message) {
+                $this->getOutput()->writeln('<error>' . $message . '</error>');
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Execute command.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->setOutput($output);
+    }
+}
